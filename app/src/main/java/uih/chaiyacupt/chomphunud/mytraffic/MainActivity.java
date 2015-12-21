b0568217d887e3bb403de6a38686b09117d959c1
chomphunud <chomphunudc@gmail.com>
2015-12-21 16:46:33 +0700

    ออกแบบ UI เสร็จแล้ว

--- app/src/main/java/uih/chaiyacupt/chomphunud/mytraffic/MainActivity.java
+++ app/src/main/java/uih/chaiyacupt/chomphunud/mytraffic/MainActivity.java
@@ -2,12 +2,27 @@ package uih.chaiyacupt.chomphunud.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    Private ListView TrafficListView;
+    Private Button aboutmeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bing widget
+        bindWidget();
+
+    }//Main Method
+    Private void bindWidget() {
+        trafficListView = (ListView) findviewById(R.id.listView)
+        aboutMebutton = (Button) findViewByID(R.id.button)
     }
-}
+
+}//Main Class
