a6289c7f383e8d1783a337c15bc4feb344adb7a6
chomphunud <chomphunudc@gmail.com>
2015-12-22 10:29:46 +0700

    ออกแบบ UI เสร็จแล้ว2

--- app/src/main/java/uih/chaiyacupt/chomphunud/mytraffic/MainActivity.java
+++ app/src/main/java/uih/chaiyacupt/chomphunud/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package uih.chaiyacupt.chomphunud.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -21,11 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         //bing widget
         bindWidget();
+        //Button Controller
+        buttonControler();
+
+    }
+
+    private void bindWidget() {
+    }//Main Class
+
+
+
+    private void buttonControler(){
+            aboutMebutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.phonton1);
+                    buttonMediaPlayer.start();
+
+                    Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                    objIntent.setData(Uri.parse("https://youtu.be/1YJ9QXe-h2A"));
+                    startActivity(objIntent);
+
+
+
+                    //event
+
+                }
+            });
+    }
 
     }//Main Method
-    Private void bindWidget() {
-        trafficListView = (ListView) findviewById(R.id.listView)
-        aboutMebutton = (Button) findViewByID(R.id.button)
-    }
 
 }//Main Class
